bf71ab06699cd3ec927012ecea4b568034ec003c
Alexander Streng <alexander.streng@avivasolutions.nl>
2019-10-14 15:49:16 +0200

    Added basic page templates.

---
+++ DancingGoatMvc/App_Start/PageBuilderComponentRegister.cs
@@ -0,0 +1,14 @@
+﻿using DancingGoat.Models.PageTemplates.LandingPage;
+using Kentico.PageBuilder.Web.Mvc.PageTemplates;
+
+[assembly: RegisterPageTemplate("LandingPageTemplate","Default Landing page template",
+    typeof(LandingPageProperties),
+    "PageTemplates/_LandingPageTemplate",
+    IconClass = "icon-l-rows-2")]
+
+namespace DancingGoat.App_Start
+{
+    public class PageBuilderComponentRegister
+    {
+    }
+}
